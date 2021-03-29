916e07d1566e4c2b22b0c7000f25a6ffd9f132a7
chaggle <dabchain431@gmail.com>
2021-03-29 13:58:42 +0800

    两天的每日一题 + 位运算

---
+++ c++/50.pow-x-n.cpp
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        double half = myPow(x, n / 2);
+        double rest = myPow(x, n % 2);
+        return rest * half * half;
+    }
+};
+// @lc code=end
+
