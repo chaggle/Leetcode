971ae4b393a7ec721af708b16e71c275ab21256b
chaggle <dabchain431@gmail.com>
2021-04-11 13:18:05 +0800

    每日一题 两日+ 动态规划与最小栈问题

---
+++ c++/264.丑数-ii.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=264 lang=cpp
+ *
+ * [264] 丑数 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        /* vector<int> factors = {2, 3, 5};
+        unordered_set<long> seen; //利用最小堆的思想进行操作
+        priority_queue<long, vector<long>, greater<long>> heap;
+        seen.insert(1L);
+        heap.push(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.top();
+            heap.pop();
+            ugly = (int)curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (!seen.count(next)) {
+                    seen.insert(next);
+                    heap.push(next);
+                }
+            }
+        }
+        return ugly; */
+        /*也可以用动态规划方式解决问题*/
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = min(min(num2, num3), num5);
+            if (dp[i] == num2) {
+                p2++;
+            }
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+};
+// @lc code=end
+
